51b81c39e211cdd649055f7f718711a98a9c97fc
amogato <167096549+amogato@users.noreply.github.com>
2025-02-10 11:53:11 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from 'npm install @react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons';
+import Tela1 from './components/Tela1';
+import Tela2 from './components/Tela2';
+
+const Tabs = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>olá!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tabs.Navigator
+        screenOptions={{
+          tabBarStyle:{
+            backgroundColor:'black',
+            height: 60
+          },
+          tabBarLabelStyle:{
+            fontSize: 22,
+            fontWeight: '500',
+            color: 'red'
+          },
+          tabBarActiveTintColor:'red'
+        }}
+      >
+        <Tabs.Screen
+          name='Tela 1'
+          component={Tela1}
+          options={{
+            tabBarIcon: ({color, size})=>(<FontAwesome name="th-large" color={color} size={size} />)
+          }}
+        />
+        <Tabs.Screen
+          name='Tela 2'
+          component={Tela2}
+        />
+      </Tabs.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
